74d91f36f643dc4aad1786034731c7293693ad10
DanielSilvado <41308458+DanielSilvado@users.noreply.github.com>
2020-10-12 15:06:23 -0300

    initial create page landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
+import './styles/global.css'
+import './styles/pages/landing.css'
+
+import logoImg from './images/logo.svg'
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Barra da Tijuca</strong>
+          <strong>Rio de Janeiro</strong>
+        </div>
+
+        <a href="" className="enter-app">
+          >
+        </a>
+      </div>
+    </div>
+
   );
 }
 
